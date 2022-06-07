415359873fff4102776f5c56c3473255e04e49f8
Matthew Pendleton <mpendle@L-R1G3GWM2F3.americas.gap.com>
2022-06-07 10:55:05 -0500

    added files that were moved

---
+++ src/main/java/com/gapinc/seri/restservice/model/BasicTopicMessage.java
@@ -0,0 +1,29 @@
+package com.gapinc.seri.restservice.model;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import org.springframework.hateoas.RepresentationModel;
+
+public class BasicTopicMessage extends RepresentationModel<BasicTopicMessage> {
+    private final Integer id;
+	private String content;
+    
+    @JsonCreator
+	public BasicTopicMessage(@JsonProperty("id") Integer id, @JsonProperty("content") String content) {
+		this.id = id;
+		this.content = content;
+	}
+
+    public Integer getId() {
+		return id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+    public void setContent(String content){
+        this.content = content;
+    }
+}
