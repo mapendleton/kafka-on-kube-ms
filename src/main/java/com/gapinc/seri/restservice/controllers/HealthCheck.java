a75e63f4a3c06f9d7d7249bffabde5fa8a03203a
Matthew Pendleton <mpendle@L-R1G3GWM2F3.americas.gap.com>
2022-06-21 14:29:53 -0500

    Added submodule for shared bash functions and modified scripts to work with wsl

--- src/main/java/com/gapinc/seri/restservice/controllers/HealthCheck.java
+++ src/main/java/com/gapinc/seri/restservice/controllers/HealthCheck.java
@@ -1,13 +1,9 @@
 package com.gapinc.seri.restservice.controllers;
 
-import com.gapinc.seri.restservice.model.BasicTopicMessage;
-import com.gapinc.seri.restservice.service.KafkaProducer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.concurrent.ExecutionException;
 
 @RestController
 public class HealthCheck {
