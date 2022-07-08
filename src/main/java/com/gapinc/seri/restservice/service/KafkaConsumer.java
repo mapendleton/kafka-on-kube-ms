e337fa1dd8f7b5fb37dd5363b0c62b22fa451a92
Matthew Pendleton <mpendle@L-R1G3GWM2F3.americas.gap.com>
2022-07-08 18:26:07 -0500

    changed listener to send basictopicmessage object

--- src/main/java/com/gapinc/seri/restservice/service/KafkaConsumer.java
+++ src/main/java/com/gapinc/seri/restservice/service/KafkaConsumer.java
@@ -9,7 +9,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 
 @Service
@@ -21,18 +20,8 @@ public class KafkaConsumer {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
     
     @KafkaListener(topics = "${spring.kafka.topic}", id = "kafkaListener")
-    //@SendTo("/topic/consumer")
     public void consume(ConsumerRecord<Integer,String> message){
-        try {
-            BasicTopicMessage btp = new BasicTopicMessage(message.key(), message.value());
-            template.convertAndSend("/topic/consumer", message.value());
-            //logger.info("Message consumed on topic: {},Key: {}, Message: {}", message.topic(),message.key(), message.value());
-            logger.info("Message consumed, Key: {}, Message: {}",btp.getId(),btp.getContent());
-            //return new BasicTopicMessage(message.key(), message.value());
-        } catch (Exception e) {
-            logger.error("my god IDK what is happening: {}", e.getMessage());
-            throw e;
-        }
-
+        template.convertAndSend("/topic/consumer", new BasicTopicMessage(message.key(), message.value()));
+        logger.info("Message consumed on topic: {},Key: {}, Message: {}", message.topic(),message.key(), message.value());
     }
 }
